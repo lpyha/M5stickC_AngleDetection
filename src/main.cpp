ce0d7f2781aa8281ab203a12c0370c1ddbc17c9e
lpyha <76113558+lpyha@users.noreply.github.com>
2023-01-24 09:51:19 +0900

    360℃表示

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@ float accX;
 float accY;
 float accZ;
 float theta;
-int degree;  
+int degree;
+
 void setup() {
   accX = 0.0F;
   accY = 0.0F;
@@ -12,7 +13,6 @@ void setup() {
   theta = 0.0F;
   degree = 0;
   M5.begin();
-  Serial.begin(9600);
   M5.IMU.Init();
   M5.Lcd.setRotation(3);
 }
@@ -20,13 +20,14 @@ void setup() {
 void loop() {
   M5.Lcd.fillScreen(BLACK);
   M5.IMU.getAccelData(&accX,&accY,&accZ);
+  //範囲π>-πまで
   theta = atan2(accY, accX);
+  theta -= PI;
+  theta = abs(theta);
   degree = theta * (180 / PI);
   Serial.println(degree);
-  //M5.Lcd.setCursor(5, 20);
-  //M5.Lcd.printf("X> %5.2f  Y> %5.2f G\n\n\n\n", accX, accY);
-  M5.Lcd.setCursor(45, 35);
-  M5.Lcd.setTextSize(6);
+  M5.Lcd.setCursor(40, 30);
+  M5.Lcd.setTextSize(5);
   M5.Lcd.printf("%d", degree);
   delay(100);
 }
